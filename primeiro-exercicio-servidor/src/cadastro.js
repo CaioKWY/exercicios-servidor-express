e52916e09528e079eb862506b269417a69930122
CaioKWY <caiokaway@gmail.com>
2023-09-17 08:34:52 -0300

    subindo exercicios

---
+++ primeiro-exercicio-servidor/src/cadastro.js
@@ -0,0 +1,23 @@
+const usuarios = [
+    {id: 11,
+    nome: "joao",
+    idade: 23
+},
+    {id: 2,
+    nome: "Maria",
+    idade: 2028
+},
+    {id: 4,
+    nome: "ana",
+    idade: 30
+},
+    {id: 1,
+    nome: "rodrigo",
+    idade: 17
+},
+    {id: 123,
+    nome: "rodrigo",
+    idade: 17}
+];
+
+module.exports = usuarios;
